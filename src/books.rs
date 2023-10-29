45c9d69b95e6b3fd64e78ef885cc2a8f638064a4
Tanja <git@tanja.pw>
2023-10-29 02:28:53 +0200

    Impl `get_img`

--- src/books.rs
+++ src/books.rs
@@ -15,6 +15,7 @@ lazy_static! {
     static ref BOOK_HTML_META_REGEX: Regex =
         Regex::new(r#"<meta name="([^"]+)"(?: |\n)+content="([^"]*)" ?\/>"#).unwrap();
     static ref BOOK_HTML_PAGE_REGEX: Regex = Regex::new(r#"\[(\d+),(\d+)\]"#).unwrap();
+    static ref IMG_REGEX: Regex = Regex::new(r#"#xlink:href="(img/[^"]+)"#).unwrap();
 }
 
 pub async fn do_book_form_dance(
@@ -215,7 +216,7 @@ pub async fn do_version_check(
     }
 }
 
-pub(crate) async fn do_download(
+pub async fn do_download(
     ApiClient(client, cookie_store): &ApiClient,
     url: &str,
     book_meta: &BookMeta,
@@ -243,6 +244,97 @@ pub(crate) async fn do_download(
         path.push(format!("{page}.svg"));
         let mut file = tokio::fs::File::create(path).await?;
         file.write_all(text.as_bytes()).await?;
+    }
+
+    Ok(())
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Img {
+    pub url: String,
+    pub page_number: usize,
+    pub img_number: usize,
+}
+
+pub async fn get_img(
+    ApiClient(client, cookie_store): &ApiClient,
+    book_meta: &BookMeta,
+    version: &Version,
+    book: &ParsedBook,
+    svg_path: impl AsRef<std::path::Path>,
+    img_path: impl AsRef<std::path::Path>,
+) -> anyhow::Result<()> {
+    let img_base_url = "https://a.digi4school.at/ebook/".to_string() + &book.id + "/";
+
+    // Image urls containing the absolute path to the image
+    let mut img_urls = Vec::new();
+
+    // Iterate over all pages (svg files in the save_path)
+    let files = std::fs::read_dir(svg_path.as_ref())?;
+
+    for file in files {
+        // Read the file
+        let file = file?;
+        let path = file.path();
+        let text = tokio::fs::read_to_string(path.clone()).await?;
+
+        // Extract the image urls
+        IMG_REGEX.captures_iter(&text).for_each(|capture| {
+            let relative_url = capture[1].to_string();
+            // relative: img/1.png
+            // absolute https://a.digi4school.at/ebook/1667/47/img/1.png
+
+            // The page number is `x` in `x.svg`
+            let page_number = path.file_stem().unwrap().to_str().unwrap();
+
+            let url = format!("{img_base_url}{page_number}/{relative_url}");
+
+            // `img_number` is the number in the relative url
+
+            let img_number = relative_url
+                .split("/")
+                .last()
+                .unwrap()
+                .split(".")
+                .next()
+                .unwrap()
+                .parse::<usize>()
+                .unwrap();
+
+            let img = Img {
+                url,
+                page_number: page_number.parse::<usize>().unwrap(),
+                img_number: 0,
+            };
+
+            img_urls.push(img);
+        });
+    }
+
+    dbg!(&img_urls[0..10]);
+
+    // Download the images
+    let path = img_path.as_ref().to_path_buf();
+    for img in img_urls {
+        let response = client.get(&img.url).send().await?;
+
+        if !response.status().is_success() {
+            return Err(anyhow::anyhow!(
+                "Got non-success status code: {}",
+                response.status()
+            ));
+        };
+
+        let bytes = response.bytes().await?;
+
+        let mut path = path.clone();
+        path.push(format!(
+            "{page_number}_{img_number}.png",
+            page_number = img.page_number,
+            img_number = img.img_number
+        ));
+        let mut file = tokio::fs::File::create(path).await?;
+        file.write_all(&bytes).await?;
     }
 
     Ok(())
